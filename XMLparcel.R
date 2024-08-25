778da3f0ea74b62e4779abc47bda176424ed4aae
Kalicz Péter <kaliczp@gmail.com>
2024-08-25 09:39:41 +0200

    Substract parcel IDs

--- XMLparcel.R
+++ XMLparcel.R
@@ -1,1 +1,4 @@
-getNodeSet(test, "/gml:FeatureCollection/gml:featureMembers/eing:FOLDRESZLETEK")
+parcels <- getNodeSet(test, "/gml:FeatureCollection/gml:featureMembers/eing:FOLDRESZLETEK")
+xmlGetAttr(parcels[[3]], "gml:id")
+
+getChildrenStrings(parcels[[3]])['GEOBJ_ID']
