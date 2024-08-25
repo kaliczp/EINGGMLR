fcbc4f0c2ad935b21928532e883e2cedeb1ac4d7
Kalicz Péter <kaliczp@gmail.com>
2024-08-25 10:55:44 +0200

    Try to select particular value

--- XMLparcel.R
+++ XMLparcel.R
@@ -2,3 +2,10 @@ parcels <- getNodeSet(test, "/gml:FeatureCollection/gml:featureMembers/eing:FOLD
 xmlGetAttr(parcels[[3]], "gml:id")
 
 getChildrenStrings(parcels[[3]])['GEOBJ_ID']
+
+ParcCurrent <- xpathApply(test, "//gmlGeobjIds", xmlValue)
+
+xpathSApply(test, "//eing:GEOBJ_ID", xmlValue) == ParcCurrent
+
+
+
