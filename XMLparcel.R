1a9153c3638bc5cf6eb2e2f850c52ce347f14954
Kalicz Péter <kaliczp@gmail.com>
2024-08-27 17:44:50 +0200

    Select current parcel

--- XMLparcel.R
+++ XMLparcel.R
@@ -7,6 +7,5 @@ ParcCurrent <- xpathApply(test, "//gmlGeobjIds", xmlValue)
 
 xpathSApply(test, "//eing:GEOBJ_ID", xmlValue) == ParcCurrent
 
-
-xpathApply(test, "//eing:FOLDRESZLETEK", function(x){xmlGetAttr(x, "gml:id")})
+xpathSApply(test, "//eing:FOLDRESZLETEK", function(x){xmlGetAttr(x, "gml:id")}) == paste0("fid-", ParcCurrent)
 
