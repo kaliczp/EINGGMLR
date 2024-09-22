5f94797ead9ce6e54b707e34293cc8ab9c4b74ac
Kalicz Péter <kaliczp@gmail.com>
2024-09-22 14:30:07 +0200

    Current parcel id are created

--- BuildNew.R
+++ BuildNew.R
@@ -1,3 +1,4 @@
+currfid <- round(abs(rnorm(1))*10^14)
 newgml <- xmlTree("gml:FeatureCollection", namespaces = list(eing = "eing.foldhivatal.hu",
                                                              gml = "http://www.opengis.net/gml",
                                                              xlink = "http://www.w3.org/1999/xlink",
@@ -9,7 +10,7 @@ newgml$setNamespace(NULL)
 newgml$addNode("MetaDataList", close = FALSE)
 newgml$addNode("gmlID", "691da01c-7911-45a7-b831-bc594bfaca16")
 newgml$addNode("gmlExportDate", round(as.numeric(Sys.time())*1000))
-newgml$addNode("gmlGeobjIds", round(abs(rnorm(1))*10^14))
+newgml$addNode("gmlGeobjIds", currfid)
 newgml$addNode("xsdVersion", 2.3)
 newgml$closeNode()
 newgml$closeNode()
@@ -19,6 +20,6 @@ gmlwithmeta <- xmlTreeParse(saveXML(newgml), useInternalNodes = T)
 root <- xmlRoot(gmlwithmeta)
 metadataNode <- newXMLNode("featureMembers", parent = root, namespace = "gml")
 parcelNode = newXMLNode("FOLDRESZLETEK", parent=metadataNode, namespace = "eing")
-xmlAttrs(parcelNode, TRUE, TRUE) <- c(id= "fid-11111111")
+xmlAttrs(parcelNode, TRUE, TRUE) <- c(id= paste0("fid-", currfid))
 boundedBy
 saveXML(gmlwithmeta, "gmlwithmeta.gml", prefix='<?xml version="1.0" encoding="UTF-8" standalone ="no"?>\n')
