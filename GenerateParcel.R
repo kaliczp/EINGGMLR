a63b28616738cf268da38cf7fe93ef5a770ab3c2
Kalicz Péter <kaliczp@gmail.com>
2024-09-25 18:13:28 +0200

    Customised parcel widht and length and parcel ID

--- GenerateParcel.R
+++ GenerateParcel.R
@@ -1,10 +1,11 @@
 library(sf)
 
 studpos <- 30
-width <- 15
-length <-80
-p1 <- rbind(c(0,0), c(width,0), c(width,length), c(0,length), c(0,0))
+parcelwidth <- sample(seq(14,20,by=0.1),1)
+parcellength <- sample(seq(75,90,by=0.1),1)
+p1 <- rbind(c(0,0), c(parcelwidth,0),
+            c(parcelwidth,parcellength), c(0,parcellength), c(0,0))
 pol1 <- st_polygon(list(p1))
 rot <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
 pol1tr <- pol1*rot(studpos * pi/40) + + c(460000, 165800)
-BuildNew(pol1tr)
+BuildNew(pol1tr, hrsz = sample(11:280,1))
