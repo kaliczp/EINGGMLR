60cb135e1f5f20101a3b479f2b70e006c02c11fc
Kalicz Péter <kaliczp@gmail.com>
2024-09-26 07:33:26 +0200

    Many parcels are generated based on students's list
    For loop on the list of students. Every students become own file. This commit closes #7.

--- GenerateParcel.R
+++ GenerateParcel.R
@@ -1,6 +1,9 @@
 library(sf)
 
-studpos <- 30
+students <- read.table("export.csv", sep = ";")
+
+for(studentnr in 1:nrow(students)){
+studpos <- studentnr + 10
 parcelwidth <- sample(seq(14,20,by=0.1),1)
 parcellength <- sample(seq(75,90,by=0.1),1)
 p1 <- rbind(c(0,0), c(parcelwidth,0),
@@ -8,4 +11,6 @@ p1 <- rbind(c(0,0), c(parcelwidth,0),
 pol1 <- st_polygon(list(p1))
 rot <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
 pol1tr <- pol1*rot(studpos * pi/40) + + c(460000, 165800)
-BuildNew(pol1tr, hrsz = sample(11:280,1))
+aktfilename <- paste0(gsub(" ", "", students[studentnr,]), ".gml")
+BuildNew(pol1tr, file = aktfilename, hrsz = sample(11:280,1))
+}
