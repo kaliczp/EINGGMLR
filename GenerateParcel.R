004817b90581e9153d863f9d5f078984f5babfcf
Kalicz Péter <kaliczp@gmail.com>
2024-10-01 13:26:04 +0200

    Coordinate shift modified
    There were a unnecessary +; deleted. Larger EOVY shift moves hopefully the place outside the current border.

--- GenerateParcel.R
+++ GenerateParcel.R
@@ -10,7 +10,7 @@ p1 <- rbind(c(0,0), c(parcelwidth,0),
             c(parcelwidth,parcellength), c(0,parcellength), c(0,0))
 pol1 <- st_polygon(list(p1))
 rot <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
-pol1tr <- pol1*rot(studpos * pi/40) + + c(460000, 165800)
+pol1tr <- pol1*rot(studpos * pi/40) + c(864000, 100000)
 aktfilename <- paste0(gsub(" ", "", students[studentnr,]), ".gml")
 BuildNew(pol1tr, file = aktfilename, hrsz = sample(11:280,1))
 }
