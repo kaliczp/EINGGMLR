3f37d786fb6a145056d41c95dd55d4901d7698ce
Kalicz Péter <kaliczp@gmail.com>
2024-10-02 17:39:42 +0200

    Three polys with randomised eastern border

--- GenerateParcel.R
+++ GenerateParcel.R
@@ -9,8 +9,12 @@ parcellength <- sample(seq(75,90,by=0.1),1)
 p1 <- rbind(c(0,0), c(parcelwidth,0),
             c(parcelwidth,parcellength), c(0,parcellength), c(0,0))
 pol1 <- st_polygon(list(p1))
+polmult <- st_sfc(pol1,
+                  pol1 + rep(c(parcelwidth, 0), 5),
+                  pol1 +  2 * rep(c(parcelwidth, 0), 5) +
+                  c(0,0,round(rnorm(1,sd = 0.1),2),0,round(rnorm(1, sd = 0.1),2),0,0,0,0,0))
 rot <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
 pol1tr <- pol1*rot(studpos * pi/40) + c(864000, 100000)
 aktfilename <- paste0(gsub(" ", "", students[studentnr,]), ".gml")
 BuildNew(pol1tr, file = aktfilename, hrsz = sample(11:280,1))
-}
+
