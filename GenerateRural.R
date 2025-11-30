258a41575f0a2ba1794a6d8aee5df0862ff513cb
Péter Kalicz <kaliczp@gmail.com>
2025-11-30 09:49:45 +0100

    Give a format to generation

--- GenerateRural.R
+++ GenerateRural.R
@@ -5,3 +5,7 @@ students <- read.table("export.csv", sep = ";", head = TRUE)
 studentnr <- 20
 
 
+{
+    parcelwidth <- sample(seq(140,200,by=0.1),1)
+    parcellength <- sample(seq(750,900,by=0.1),1)
+}
